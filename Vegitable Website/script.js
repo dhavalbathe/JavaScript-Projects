2786871d50f30da04892e234a214e863fb149ebc
dhavalbathe <dhavalbathe@gmail.com>
2024-12-28 17:21:21 +0530

    images and responsiveness is done

--- Vegitable Website/script.js
+++ Vegitable Website/script.js
@@ -1,21 +1,41 @@
 let searchForm = document.querySelector(".search-form");
 let shoppingCart = document.querySelector('.shopping-cart');
 let loginBtn = document.querySelector('.login-form');
+let navbar = document.querySelector('.navbar');
+
 
 document.querySelector("#search-btn").onclick = () => {
     searchForm.classList.toggle("active");
     shoppingCart.classList.remove('active');
     loginBtn.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 document.querySelector('#cart-btn').onclick = () => {
     shoppingCart.classList.toggle('active');
     loginBtn.classList.remove('active');
     searchForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 document.querySelector('#login-btn').onclick = () => {
     loginBtn.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginBtn.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginBtn.classList.remove('active');
     shoppingCart.classList.remove('active');
     searchForm.classList.remove('active');
 }
